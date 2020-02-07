8420ed4df55b19ccbd63fa7f25d67d1a82eaf9ce
joker <jokerblack2508826394@gmail.com>
2020-02-07 18:08:53 +0800

    1

--- chap12_exa1/src/main/java/handler/server/OneIn.java
+++ chap12_exa1/src/main/java/handler/server/OneIn.java
@@ -15,9 +15,13 @@ public class OneIn extends ChannelInboundHandlerAdapter
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception
     {
+        System.out.println("qwer");
+        // System.out.println(msg.getClass().getName());
+        /*
         HttpObject httpObject = (HttpObject) msg;
         FullHttpRequest request = (FullHttpRequest) httpObject;
         String uri = request.uri();
-        WebSocketServerHandshakerFactory handshakerFactory = new WebSocketServerHandshakerFactory();
+        WebSocketServerHandshakerFactory handshakerFactory = new WebSocketServerHandshakerFactory("ws://127.0.0.1:8189/ws", null, false);
+        */
     }
 }
